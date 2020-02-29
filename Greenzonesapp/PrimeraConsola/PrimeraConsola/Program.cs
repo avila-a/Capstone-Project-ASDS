36bc5b3b5c9df00563d6d82313a2c343ea30b622
Antonio Avila <55975309+avila-a@users.noreply.github.com>
2020-02-29 19:24:21 +0000

    Major changes
    Advanced in:
    - Download Eurostat (include France in EMP, GVA and GDP)
    - Create SQL db to structure my data
    - Analysis LEZ: Start to have some idea on the models to use
    - Greenzones app: Include analysis of app (to further explore)

---
+++ Greenzonesapp/PrimeraConsola/PrimeraConsola/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+
+
+namespace PrimeraConsola2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key_raw = Console.ReadLine();
+            string salt_raw = Console.ReadLine();
+            Console.WriteLine(new Rfc2898DeriveBytes(key_raw, Encoding.ASCII.GetBytes(salt_raw)));
+        }
+    }
+}
